687f8e50bbdc34317e7aa65e443417d9932088f7
webmasterpf-DG <webmaster@provence-formation.com>
2017-07-06 12:08:20 +0200

    ER: page actus globales, conversion en Sass compliquée

--- node/node-page_actualites_globales.tpl.php
+++ node/node-page_actualites_globales.tpl.php
@@ -6,15 +6,10 @@
 <div class="node <?php print $classes; ?>" id="node-<?php print $node->nid; ?>">
     <div class="node-inner">
         <!--______________COLONNE 1________________ -->
-        <?php /* choix du layout selon nombre de colonne
-         * .col1_layout_200_590_200{} .col1_layout_330_all{} .col1_layout_18_56_25{} .col1_layout_370_250_370{} .col1_layout_730_250{}
-         * .col2_layout_200_590_200{} .col2_layout_330_all{} .col2_layout_18_56_25{} .col2_layout_370_250_370{} .col2_layout_730_250{}
-         * .col3_layout_200_590_200{} .col3_layout_330_all{} .col3_layout_18_56_25{} .col3_layout_370_250_370{}
-         * Possible 2 colonnes avec derniere option;alors supprimer colonne-3
-         */?>
-        <div id="colonne-1" class="col1_layout_375_375_220">
+
+        <div id="colonne-1" class="col1_layout_4_5_3 page-actualites">
             <?php if ($title): /*copier le titre dans la colonne desirée*/?>
-            <h1 class="titre_page"><?php print $title; ?></h1>
+            <h1 class="titre_page_actualites"><?php print $title; ?></h1>
             <?php endif; ?>
 
                   <?php print $picture; ?>
@@ -27,33 +22,32 @@
                 <?php   print $node->content['body']['#value'];/*déplacer le contenu dans la colonne désirée*/ ?>
             </div>
 
-            
           <?php
-$theme_path = drupal_get_path('theme', 'cyrano_er');
-include ($theme_path.'/includes/inc_region_col_G1.php');
-?>
+           global $theme_path;
+              include ($theme_path.'/includes/regions_inc/inc_region_col_1.php');
+              ?>
         </div>
         <!--______________COLONNE 2________________ -->
          <!-- <pre> <?php //print_r($node); ?> </pre>-->   <!-- listage des variables du $content -->
-        <div id="colonne-2" class="col2_layout_375_375_220">
+        <div id="colonne-2" class="col2_layout_4_5_3 page-actualites">
 
     
 
-             <?php
-$theme_path = drupal_get_path('theme', 'cyrano_er');
-include ($theme_path.'/includes/inc_region_col_G2.php');
-?>
+         <?php
+           global $theme_path;
+              include ($theme_path.'/includes/regions_inc/inc_region_col_2.php');
+              ?>
 
 
 
         </div>
         
         <!--______________COLONNE 3________________ -->
-        <div id="colonne-3" class="col3_layout_375_375_220">
- <?php
-$theme_path = drupal_get_path('theme', 'cyrano_er');
-include ($theme_path.'/includes/inc_region_col_G3.php');
-?>
+        <div id="colonne-3" class="col3_layout_4_5_3 page-actualites">
+      <?php
+           global $theme_path;
+              include ($theme_path.'/includes/regions_inc/inc_region_col_3.php');
+              ?>
 
 
         </div>
