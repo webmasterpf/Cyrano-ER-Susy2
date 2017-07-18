7f42728bff71ba946b08c130d9c3a4ad7d10534a
webmasterpf-DG <webmaster@provence-formation.com>
2017-07-18 16:33:25 +0200

    ER: page archives , ajustement bouton filtrage

--- node/node-page_vdl_liste.tpl.php
+++ node/node-page_vdl_liste.tpl.php
@@ -6,18 +6,17 @@
 <div class="node <?php print $classes; ?>" id="node-<?php print $node->nid; ?>">
     <div class="node-inner">
         <!--______________COLONNE 1________________ -->
-        <div id="colonne-1" class="col1_layout_2_6_4 vdl_liste">
+        <div id="colonne-1" class="col1_layout_3_6_3 vdl_liste ">
             <?php if ($title): /*copier le titre dans la colonne desirée*/?>
             <h1 class="titre_page_vdl"><?php print $title; ?></h1>
             <?php endif; ?>
-             <?php
+         <?php
            global $theme_path;
               include ($theme_path.'/includes/regions_inc/inc_region_col_1.php');
               ?>
         </div>
         <!--______________COLONNE 2________________ -->
-         <!-- <pre> <?php //print_r($node); ?> </pre>-->   <!-- listage des variables du $content -->
-        <div id="colonne-2" class="col2_layout_18_56_25 vdl_liste">
+                 <div id="colonne-2" class="col2_layout_3_6_3 vdl_liste ">
 
             <?php print $picture; ?>
 
@@ -27,25 +26,34 @@
 
             <div class="content">
                 <?php   print $node->content['body']['#value'];/*déplacer le contenu dans la colonne désirée*/ ?>
-             
+
        
-              <?php
+                <?php if ($node->field_choix_archives[0]['view']): ?>
+            <div id="les-archives">
+                    <?php  print $node->field_choix_archives[0]['view']  ?>
+            </div>
+            <?php endif;?>
+                
+                
+                         <?php
            global $theme_path;
               include ($theme_path.'/includes/regions_inc/inc_region_col_2.php');
               ?>
-                
             </div>
 
         </div>
 
         <!--______________COLONNE 3________________ -->
-        <div id="colonne-3" class="col3_layout_18_56_25">
-                             <?php
+        <div id="colonne-3" class="col3_layout_3_6_3 vdl_liste ">
+          
+                         <?php
            global $theme_path;
               include ($theme_path.'/includes/dedicates_inc/inc_rostand_actus.php');
               ?>
-        
-
+         <?php
+           global $theme_path;
+              include ($theme_path.'/includes/regions_inc/inc_region_col_3.php');
+              ?>
         </div>
             <!--______________LIENS MENU et TAXO________________ -->
         <?php if ($terms): ?>
